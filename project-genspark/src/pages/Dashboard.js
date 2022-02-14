0b6361d31089544dd636342a6860dfe17fa5bf08
bradfan <bradley.tomlinson@genspark.com>
2022-02-14 14:52:19 -0700

    work on input logic, css

--- project-genspark/src/pages/Dashboard.js
+++ project-genspark/src/pages/Dashboard.js
@@ -1,14 +1,78 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function Dashboard() {
-    return (
-        <div className="dashboard">
-         <div className="dashboard-main">
-         <input className="input-dashboard" type="text" placeholder="Enter info here"></input>
-         <button className="btn btn-dashboard btn-submit" type="submit" >SUBMIT</button>
-         </div>
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setFirstName(value);
+   console.log("Name:", firstName);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="dashboard">
+      <div className="dashboard-input">
+        <input
+          className="input-dashboard"
+          type="text"
+          value={firstName}
+          onChange={handleInputChange}
+          placeholder="Enter first name here"
+        ></input>
+
+      
+        <input
+          className="input-dashboard"
+          type="text"
+          value={lastName}
+          onChange={handleInputChange}
+          placeholder="Enter last name here"
+        ></input>
+
+        
+        <input
+          className="input-dashboard"
+          type="text"
+          value={phoneNumber}
+          onChange={handleInputChange}
+          placeholder="Enter phone number here"
+        ></input>
+
+        <button
+          className="btn btn-dashboard btn-submit"
+          value={phoneNumber}
+          onClick={(e) => {
+            if (firstName === "") {
+              return alert("Please enter your phone number.");
+            }
+            onSubmit(e);
+          }}
+          type="submit"
+        >
+          SUBMIT
+        </button>
+
+      </div>
+
+      <div className="container">
+        <div className="box box-one">
+          <h1>{firstName}</h1>
         </div>
-    )
+        <div className="box box-two">
+        <h1>{lastName}</h1>
+        </div>
+        <div className="box box-three">
+          <h1>{phoneNumber}</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
