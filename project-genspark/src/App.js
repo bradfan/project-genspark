fa6db10e2ebb800cbe9822bf6c4d58520c19292b
bradfan <bradley.tomlinson@genspark.com>
2022-02-14 13:07:08 -0700

    establish dashboard/footer

--- project-genspark/src/App.js
+++ project-genspark/src/App.js
@@ -1,11 +1,15 @@
 
 import './index.css';
 import Header from "./pages/Header"
+import Dashboard from "./pages/Dashboard"
+import Footer from "./pages/Footer"
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Dashboard/>
+     <Footer/>
     </div>
   );
 }
---
+++ project-genspark/src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Dashboard() {
+    return (
+        <div className="dashboard">
+         <div className="dashboard-main">
+         <input className="input-dashboard" type="text" placeholder="Enter info here"></input>
+         <button className="btn btn-dashboard btn-submit" type="submit" >SUBMIT</button>
+         </div>
+        </div>
+    )
+}
+
+export default Dashboard
