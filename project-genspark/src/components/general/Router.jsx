67ef3cf37df9080abd9951f8fa0c8a2f5d28123b
bradfan <bradley.tomlinson@genspark.com>
2022-02-15 14:48:25 -0700

    Error/gitignore

--- project-genspark/src/components/general/Router.jsx
+++ project-genspark/src/components/general/Router.jsx
@@ -1,26 +1,31 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "../pages/Header";
-
+import Login from "../general/Login";
+import Welcome from "../general/Welcome";
 import Footer from "../pages/Footer";
 
 class RouterComponent extends Component {
-    render() {
-        return(
-            <div>
-             <Router>
-                 <Header />
-                     {/* <Switch>
-                         <Route exact path="/"><WelcomeComponent name="Everett"/></Route>                       
-                         <Route path="/theEmployee/:id" component={AddEmployee} />
-                         <Route path="/employee/:id/:jobTitle" component={UpdateEmployeeComponent} />
-                         <Route path="/EmployeeRegistry" exact component={EmployeeRegistryComponent} />
-                     </Switch> */}
-                 <Footer /> 
-             </ Router>
-             </div>
-        )
-    } 
- }
- 
- export default RouterComponent; 
+  render() {
+    return (
+      <div>
+        <Router>
+          <Header />
+          <Switch>
+              
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route exact path="/">
+              <Welcome name="Everett" />
+            </Route>
+        
+          </Switch>
+          <Footer />
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default RouterComponent;
